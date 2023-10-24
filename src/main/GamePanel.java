2cac52b781edef634fed56cce3dbc51af77bbe11
AlexandreModesto <xandy.modest14@gmail.com>
2023-10-24 14:59:14 -0300

    first commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,77 @@
+package main;
+
+import Entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    //Screen settings
+    final int originalTileSize = 16;//16x16 tile
+    final int scale =3;
+
+    public final int tileSize = originalTileSize * scale;//48x48
+    final int maxScreenCol = 16;
+    final int maxScreenRow=12;
+    final int screenWidth = tileSize * maxScreenCol;//768px
+    final int screenHeight = tileSize* maxScreenRow;//576px
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    Player player = new Player(this,keyH);
+    //Set res.player default position
+    int playerX = 100;
+    int playerY=100;
+    int playerSpeed=4;
+
+    //FPS
+    int FPS=60;
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        gameThread=new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta=0;
+        long  lastTime=System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null){
+            currentTime=System.nanoTime();
+            delta+=(currentTime-lastTime)/drawInterval;
+            lastTime=currentTime;
+
+            if(delta>=1){
+                update();
+                repaint();
+                delta--;
+
+            }
+        }
+    }
+
+    public void update(){
+        player.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        player.draw(g2);
+        g2.dispose();
+    }
+
+
+}
